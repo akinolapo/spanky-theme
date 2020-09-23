a5d60a008a710587faa5f9b556941b1c05e41ff7
Akinola Peace Olamide <60118187+akinolapo@users.noreply.github.com>
2020-09-23 04:03:20 +0100

    Add files via upload

---
+++ js/menu-script.js
@@ -0,0 +1,28 @@
+function myMobileFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myTopMenuFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function (event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
